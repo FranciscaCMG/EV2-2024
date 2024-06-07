35691341d65741d72f6f62220482c523d84a2e39
FranciscaCMG <francisca.marquez@usach.cl>
2024-06-07 15:01:21 -0400

    Carpetas

---
+++ ms-listaReparaciones/src/main/java/cl/usach/ms_listaReparaciones/MsListaReparacionesApplication.java
@@ -0,0 +1,13 @@
+package cl.usach.ms_listaReparaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsListaReparacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsListaReparacionesApplication.class, args);
+	}
+
+}
