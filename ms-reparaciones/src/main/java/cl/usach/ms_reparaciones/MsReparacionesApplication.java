147d8a72b458c7547d6e585011f90ddc4f1e2e0d
FranciscaCMG <francisca.marquez@usach.cl>
2024-06-07 22:59:56 -0400

    Agrego microservicio Reparaciones

---
+++ ms-reparaciones/src/main/java/cl/usach/ms_reparaciones/MsReparacionesApplication.java
@@ -0,0 +1,13 @@
+package cl.usach.ms_reparaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsReparacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsReparacionesApplication.class, args);
+	}
+
+}
