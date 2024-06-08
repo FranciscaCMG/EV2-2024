5c7154674c46d65ff87e164e25b8612d0e9fe88e
FranciscaCMG <francisca.marquez@usach.cl>
2024-06-08 00:21:05 -0400

    feign

---
+++ ms-vehiculos/src/main/java/cl/usach/ms_vehiculos/configurations/FeignClientConfig.java
@@ -0,0 +1,13 @@
+package cl.usach.ms_vehiculos.configurations;
+
+import org.springframework.context.annotation.Configuration;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignClientConfig {
+    @Bean
+    Logger.Level feignLoggerLevel() { return Logger.Level.FULL; }
+}
